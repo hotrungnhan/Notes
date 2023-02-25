95b1ec11e42153275ea7706d61eb5dd3bc939a32
Hồ Trung Nhân <hotrungnhan29@gmail.com>
2023-02-26 01:03:04 +0700

    update

--- lib/app.dart
+++ lib/app.dart
@@ -16,16 +16,17 @@ class MyApp extends StatelessWidget {
             builder: (context) => PlatformApp.router(
                   debugShowCheckedModeBanner: false,
                   routerConfig: router,
-                  // builder: (context, child) => ResponsiveWrapper.builder(child,
-                  //     maxWidth: 1200,
-                  //     minWidth: 480,
-                  //     defaultScale: true,
-                  //     breakpoints: [
-                  //       const ResponsiveBreakpoint.resize(480, name: MOBILE),
-                  //       const ResponsiveBreakpoint.autoScale(800, name: TABLET),
-                  //       const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-                  //     ],
-                  //     background: Container(color: const Color(0x00ffffff))),
+                  builder: (context, child) => ResponsiveWrapper.builder(child,
+                      maxWidth: 1280,
+                      minWidth: 480,
+                      defaultScale: true,
+                      breakpoints: [
+                        const ResponsiveBreakpoint.resize(480, name: MOBILE),
+                        const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+                        const ResponsiveBreakpoint.autoScale(1280,
+                            name: DESKTOP),
+                      ],
+                      background: Container(color: const Color(0x00ffffff))),
                   localizationsDelegates: const <
                       LocalizationsDelegate<dynamic>>[
                     DefaultMaterialLocalizations.delegate,
